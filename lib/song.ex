285c7c528a666a1dc7bc6528bf28a46cd0788552
Fabrice Nourisson <fnourisson@servebox.com>
2016-04-26 16:13:09 +0200

    Split multiline into single line

--- lib/song.ex
+++ lib/song.ex
@@ -10,99 +10,76 @@ defmodule SONG do
   defp lyrics([head|tail], acc) do
     lyrics(tail, [verse(head) | acc] )
   end
-  
-  defp verse(1) do
-  """
-There was an old woman who swallowed a fly,
-I don't know why she swallowed a fly,
-Perhaps she'll die.
 
-  """
+  defp verse(1) do
+    "There was an old woman who swallowed a fly,\n" <>
+      "I don't know why she swallowed a fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(2) do
-  """
-There was an old woman who swallowed a spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-  """
+    "There was an old woman who swallowed a spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(3) do
-  """
-There was an old woman who swallowed a bird,
-How absurd! to swallow a bird,
-She swallowed the bird to catch the spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-  """
+    "There was an old woman who swallowed a bird,\n" <>
+      "How absurd! to swallow a bird,\n" <>
+      "She swallowed the bird to catch the spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(4) do
-    """
-There was an old woman who swallowed a cat,
-Imagine that! to swallow a cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-    """
+    "There was an old woman who swallowed a cat,\n" <>
+      "Imagine that! to swallow a cat,\n" <>
+      "She swallowed the cat to catch the bird,\n" <>
+      "She swallowed the bird to catch the spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(5) do
-    """
-There was an old woman who swallowed a dog,
-What a hog! to swallow a dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-    """
+    "There was an old woman who swallowed a dog,\n" <>
+      "What a hog! to swallow a dog,\n" <>
+      "She swallowed the dog to catch the cat,\n" <>
+      "She swallowed the cat to catch the bird,\n" <>
+      "She swallowed the bird to catch the spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(6) do
-    """
-There was an old woman who swallowed a goat,
-Just opened her throat! to swallow a goat,
-She swallowed the goat to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-    """
+    "There was an old woman who swallowed a goat,\n" <>
+      "Just opened her throat! to swallow a goat,\n" <>
+      "She swallowed the goat to catch the dog,\n" <>
+      "She swallowed the dog to catch the cat,\n" <>
+      "She swallowed the cat to catch the bird,\n" <>
+      "She swallowed the bird to catch the spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(7) do
-    """
-There was an old woman who swallowed a cow,
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat,
-She swallowed the goat to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-That wriggled and jiggled and tickled inside her,
-She swallowed the spider to catch the fly,
-I don't know why she swallowed the fly,
-Perhaps she'll die.
-
-    """
+    "There was an old woman who swallowed a cow,\n" <>
+      "I don't know how she swallowed a cow!\n" <>
+      "She swallowed the cow to catch the goat,\n" <>
+      "She swallowed the goat to catch the dog,\n" <>
+      "She swallowed the dog to catch the cat,\n" <>
+      "She swallowed the cat to catch the bird,\n" <>
+      "She swallowed the bird to catch the spider,\n" <>
+      "That wriggled and jiggled and tickled inside her,\n" <>
+      "She swallowed the spider to catch the fly,\n" <>
+      "I don't know why she swallowed the fly,\n" <>
+      "Perhaps she'll die.\n\n"
   end
   defp verse(8) do
-    """
-There was an old woman who swallowed a horse,
-She's dead—of course!
-  """
+    "There was an old woman who swallowed a horse,\n" <>
+      "She's dead—of course!\n"
   end
 end
