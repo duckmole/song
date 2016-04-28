779541bc6a8e129ff6a3d146c3aa93410b39cb66
Fabrice Nourisson <fnourisson@servebox.com>
2016-04-28 09:41:54 +0200

    refactor verse

--- lib/song.ex
+++ lib/song.ex
@@ -7,55 +7,31 @@ defmodule SONG do
   end
 
   defp lyrics(all_animals, [], acc) do
-    final_acc = [verse(length(all_animals), all_animals) | acc]
+    final_acc = [verse(all_animals, :last) | acc]
     List.foldl(final_acc, "", &(&1 <> &2))
   end
   defp lyrics(current_swallowed, [next_swallowed | t], acc) do
     next = [next_swallowed | current_swallowed]
-    lyrics(next, t, [verse(length(current_swallowed), current_swallowed) | acc] )
+    lyrics(next, t, [verse(current_swallowed, :not_last) | acc] )
   end
 
-  defp verse(1, swallowed_animals=[h]) do
+  defp verse(swallowed_animals=[h|_], :last) do
     begin_verse(h) <>
-    end_verse(swallowed_animals)
+    end_verse(swallowed_animals, :last)
   end
-  defp verse(2, swallowed_animals=[h|t]) do
+  defp verse(swallowed_animals=[h], step) do
     begin_verse(h) <>
-      end_verse(swallowed_animals)
+    end_verse(swallowed_animals, step)
   end
-  defp verse(3, swallowed_animals=[h|t]) do
+  defp verse(swallowed_animals=[h,_], step) do
     begin_verse(h) <>
-      exclamation(h) <>
-      swallowed_animals(swallowed_animals) <>
-      end_verse(swallowed_animals)
+      end_verse(swallowed_animals, step)
   end
-  defp verse(4, swallowed_animals=[h|t]) do
+  defp verse(swallowed_animals=[h|_], step) do
     begin_verse(h) <>
       exclamation(h) <>
       swallowed_animals(swallowed_animals) <>
-      end_verse(swallowed_animals)
-  end
-  defp verse(5,swallowed_animals=[h|t]) do
-    begin_verse(h) <>
-      exclamation(h) <>
-      swallowed_animals(swallowed_animals) <>
-      end_verse(swallowed_animals)
-  end
-  defp verse(6, swallowed_animals=[h|t]) do
-    begin_verse(h) <>
-      exclamation(h) <>
-      swallowed_animals(swallowed_animals) <>
-      end_verse(swallowed_animals)
-  end
-  defp verse(7, swallowed_animals=[h|t]) do
-    begin_verse(h) <>
-      exclamation(h) <>
-      swallowed_animals(swallowed_animals) <>
-      end_verse(swallowed_animals)
-  end
-  defp verse(8, swallowed_animals= [h|t]) do
-    "There was an old woman who swallowed a horse,\n" <>
-      "She's dead—of course!\n"
+      end_verse(swallowed_animals, step)
   end
 
   defp swallow(predator, target) do
@@ -81,11 +57,14 @@ defmodule SONG do
     "That wriggled and jiggled and tickled inside her,\n"
   end
 
-  defp end_verse([first]) do
+  defp end_verse(_, :last) do
+    "She's dead—of course!\n"
+  end
+  defp end_verse([first], _) do
     "I don't know why she swallowed a #{first},\n" <>
     "Perhaps she'll die.\n\n"
   end
-  defp end_verse(animals) do
+  defp end_verse(animals, _) do
     [first, second | _] = Enum.reverse(animals)
     middle_verse() <>
       swallow(second, first) <>
